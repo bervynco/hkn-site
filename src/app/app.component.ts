c54ed4e35c0139b4ae62f954cfe2e0d8852e0bd2
Shahzaib Saeed <shahzaibsaeed077@gmail.com>
2025-05-11 07:35:04 -0700

    fix footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/share/header/header.component";
 import { FooterComponent } from "./components/share/footer/footer.component";
-import { isPlatformBrowser } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { Inject, PLATFORM_ID } from '@angular/core';
 
 
@@ -10,13 +10,19 @@ import { Inject, PLATFORM_ID } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+     isBrowser: boolean = false;
+
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  
+  ngOnInit(): void {
+    this.isBrowser = isPlatformBrowser(this.platformId);  
+  }
 
   scrollToTop() {
     if (isPlatformBrowser(this.platformId)) {
--- src/app/components/share/footer/footer.component.ts
+++ src/app/components/share/footer/footer.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
 
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  isBrowser: boolean = false;
+   constructor(
+
+    @Inject(PLATFORM_ID) private platformId: any
+  ) {}
+  ngOnInit(): void {
+    this.isBrowser = isPlatformBrowser(this.platformId);  
+  }
 
 }
